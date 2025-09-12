e6be261465aa272900c298422351fe8a88e80b80
Daniel Ariaz-Avalos <dannyariaz45@gmail.com>
2025-09-12 13:16:12 -0700

    (updated) 2. Create int vector maze. Function to create and print maze

--- main.cpp
+++ main.cpp
@@ -3,15 +3,13 @@
 #include <string>
 #include <vector>
 
-int main() {
-
+std::vector<std::vector<int>> createMaze(const std::string& fileName) {
     std::vector<std::vector<int>> maze;
     std::ifstream file("maze0-1_input.txt");
     std::string line;
 
     if (file.is_open()) {
         while (std::getline(file, line)) {
-
             std::vector<int> row;
             for (char c : line) {
                 row.push_back(c - '0');
@@ -23,14 +21,23 @@ int main() {
     else {
         std::cout << "Unable to open file" << std::endl;
     }
+    return maze;
+}
 
+void printMaze(const std::vector<std::vector<int>>& maze) {
     for (const auto& row : maze) {
         for (int cell : row) {
             std::cout << cell;
         }
         std::cout << std::endl;
     }
+}
 
+
+int main() {
+
+    std::vector<std::vector<int>> maze = createMaze("maze0-1_input.txt");
+    printMaze(maze);
 
     return 0;
 }
