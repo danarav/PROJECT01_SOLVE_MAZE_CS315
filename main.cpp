a35deb03a96bc98e01ea46a715b9326846007adf
Daniel Ariaz-Avalos <dannyariaz45@gmail.com>
2025-09-12 13:50:07 -0700

    3. Function to find START cell and TARGET cell. Returns pair<int, int>

--- main.cpp
+++ main.cpp
@@ -33,11 +33,48 @@ void printMaze(const std::vector<std::vector<int>>& maze) {
     }
 }
 
+std::pair<int, int> findStart(const std::vector<std::vector<int>>& maze) {
+    for (int row = 0; row < maze.size(); ++row) {
+        if (maze[row][0] == 0) {
+            return std::make_pair(row, 0);
+        }
+    }
+
+    for (int col = 0; col < maze[0].size(); ++col) {
+        if (maze[0][col] == 0) {
+            return std::make_pair(col, 0);
+        }
+    }
+
+    std::cout << "No start position was located. RETURNING (-1, -1). " << std::endl;
+    return std::make_pair(-1, -1);
+}
+
+std::pair<int, int> findTarget(const std::vector<std::vector<int>>& maze) {
+    for (int row = 0; row < maze.size(); ++row) {
+        if (maze[row][maze[0].size() - 1] == 0) {
+            return std::make_pair(row, maze[0].size() - 1);
+        }
+    }
+
+    for (int col = 0; col < maze[0].size(); ++col) {
+        if (maze[maze.size() - 1][col] == 0) {
+            return std::make_pair(maze.size() - 1, col);
+        }
+    }
+
+    std::cout << "No tarrget was located. RETURNING (-1, -1). " << std::endl;
+    return std::make_pair(-1, -1);
+}
 
 int main() {
-
     std::vector<std::vector<int>> maze = createMaze("maze0-1_input.txt");
     printMaze(maze);
 
+    std::pair<int, int> start = findStart(maze);
+    std ::cout << "Start position: " << start.first << ", " << start.second << std::endl;
+
+    std::pair<int, int> target = findTarget(maze);
+    std ::cout << "Target position: " << target.first << ", " << target.second << std::endl;
     return 0;
 }
